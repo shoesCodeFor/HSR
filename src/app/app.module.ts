0b11e77cd1b365e608257495a41cb6a429b4505d
Schuyler Ankele <schuyler.ankele@gmail.com>
2017-09-25 23:12:43 -0600

    Refactor, rebuild and retry :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+const appRoutes: Routes = [
+   {path: '', component: HomeComponent},
+   {path: 'about', component: AboutComponent}
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
